33c961fd5753723626eb1adb46472771d734e94e
kerstin99forster <72085426+kerstin99forster@users.noreply.github.com>
2021-07-10 22:39:40 +0200

    Update for general forest datasets

--- sequential_comparison.m
+++ sequential_comparison.m
@@ -23,46 +23,35 @@ switch(environment)
             I2_name = images(i+1).name;
             I2 = imread(I2_name);
             [I1_prepro, I2_prepro] = preprocessing_slow(i, I1, I2);
-            change_detection_forest(i, I1_prepro, I2_prepro, I1_name, I2_name);
-        end
-    case 'Glacier'
-        for i = 1:(num_images-1)
-            I2_name = images(i+1).name;
-            I2 = imread(I2_name);
-            [I1_prepro, I2_prepro] = preprocessing_glacier(I1, I2);
-            change_detection_glacier(i, I1_prepro, I2_prepro, I1_name, I2_name);
-        end
-    case 'Tire Graveyard'
-        for i = 1:(num_images-1)
-            I2_name = images(i+1).name;
-            I2 = imread(I2_name);
-            [I1_prepro, I2_prepro] = preprocessing_tire(I1, I2);
             if i == 1
                 ref = change_detection_tire_ref(I1_prepro, I1_name);
             end
             change_detection_tire(I1_prepro, I2_prepro, I2_name, ref);
         end
-    case 'Dubai'
-        change_detection_dubai(path);
-    case 'Kuwait'
-        change_detection_kuwait(path);
-    case 'Frauenkirche'
+    case 'Tire Graveyard'
         for i = 1:(num_images-1)
             I2_name = images(i+1).name;
             I2 = imread(I2_name);
-            [I1_prepro, I2_prepro] = preprocessing_frauenkirche(i, I1, I2);
-            % only for development purposes
-            figure, imshowpair(I1_prepro, I2_prepro,'montage')
-            % change_detection_frauenkirche(i, I1_prepro, I2_prepro, I1_name, I2_name);
+            if i == 1
+                ref = change_detection_tire_ref(I1, I1_name);
+            end
+            change_detection_tire(I1, I2, I2_name, ref);
         end
+    case 'Frauenkirche'
+        change_detection_frauenkirche(path);
     case 'Wiesn'
         for i = 1:(num_images-1)
             I2_name = images(i+1).name;
             I2 = imread(I2_name);
             [I1_prepro, I2_prepro] = preprocessing_wiesn(I1, I2);
-            % only for development purposes
-            figure, imshowpair(I1_prepro, I2_prepro,'montage')
-            %change_detection_wiesn(i, I1_prepro, I2_prepro, I1_name, I2_name);
+            change_detection_wiesn(i, I1_prepro, I2_prepro, I1_name, I2_name);
         end
+    case 'Glacier'
+        change_detection_glacier(path);
+    case 'Dubai'
+        change_detection_dubai(path);
+    case 'Kuwait'
+        change_detection_kuwait(path);
+
 end
 
