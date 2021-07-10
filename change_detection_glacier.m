08a8d79f8a576630b4e61eb0c64407de78030684
philipsidu <86351832+philipsidu@users.noreply.github.com>
2021-07-10 11:34:10 +0200

    Add files via upload

---
+++ change_detection_glacier.m
@@ -0,0 +1,28 @@
+function change_detection_glacier(i, I1_prepro, I2_prepro, I1_name, I2_name)
+%CHANGE_DETECTION_GLACIER Summary of this function goes here
+%   Detailed explanation goes here
+
+% Grayscale & Binarize Image
+if i == 1
+    % Setting first Image as Reference Image
+    ref = rgb2gray(I1_prepro);
+    bwImage = imbinarize(ref,0.7);
+    % Saving
+    dif_name = erase(I1_name, '0a_');
+    dif_name = erase(dif_name, '.jpg');
+    filename = [dif_name, '_TireGraveyard.jpg'];
+    imwrite(I1_prepro, ['processedData/',filename]);
+end
+
+recovered = rgb2gray(I2_prepro);
+bwImage2 = imbinarize(recovered,0.7);
+
+%Difference Comparison of Glacier
+c = imfuse(bwImage,bwImage2,"ColorChannels","red");
+dif = imfuse(I1_prepro,c,'blend','Scaling', 'joint');
+
+%Saving
+dif_name = erase(I2_name, '.jpg');
+imwrite(dif, [dif_name, '_dif.jpg']);
+end
+
