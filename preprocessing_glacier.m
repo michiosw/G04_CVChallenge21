193651838f66b3ecdc552a258735145f323af32c
kerstin99forster <72085426+kerstin99forster@users.noreply.github.com>
2021-07-10 17:43:57 +0200

    Ultra sicke preprocessing glacier function

--- preprocessing_glacier.m
+++ preprocessing_glacier.m
@@ -7,21 +7,37 @@ function [I1_prepro, I2_prepro] = preprocessing_glacier(I1, I2)
 %   Output: Preprocessed reference image, preprocessed comparison image
 %   (aligned in terms of scale/rotation/translation)
 
-% works for Columbia Glacier
+% Successful tests:
+% Glacier
+% Dubai
+% Kuwait (also 2015-2018)
+% Wiesn (also 2015-2021)
+% Frauenkirche 2012_08-2015_08 / 2012-2018
 
-% 1. Image Sharpening
+% Unsuccessful tests:
+% Rainforest
+% Dubai 1990-2020
 
-I1_sharp = imsharpen(I1);
-I2_sharp = imsharpen(I2);
+% 1. Histogram Equalization for Contrast Alignment
 
-% 2. Reversing scale, rotation, and translation differences in the set using:
-% 2.1 Feature detection/matching (SURF) [1]
+I1_eq = histeq(I1);
+I2_eq = histeq(I2);
 
-I1_gray = rgb2gray(I1_sharp);
-I2_gray = rgb2gray(I2_sharp);
+% 2. Flat-Field Correction (Gaussian Smoothing) to correct shading
 
-ptsOriginal  = detectSURFFeatures(I1_gray);
-ptsDistorted = detectSURFFeatures(I2_gray);
+sigma = 30;
+
+I1_flat = imflatfield(I1_eq,sigma);
+I2_flat = imflatfield(I2_eq,sigma);
+
+% 3. Reversing scale, rotation, and translation differences in the set using:
+% 3.1 Feature detection/matching (SURF) [1]
+
+I1_gray = rgb2gray(I1_flat);
+I2_gray = rgb2gray(I2_flat);
+
+ptsOriginal  = detectSURFFeatures(I1_gray, 'MetricThreshold', 100);
+ptsDistorted = detectSURFFeatures(I2_gray, 'MetricThreshold', 100);
 
 [featuresOriginal,  validPtsOriginal]  = extractFeatures(I1_gray,  ptsOriginal);
 [featuresDistorted, validPtsDistorted] = extractFeatures(I2_gray, ptsDistorted);
@@ -31,17 +47,16 @@ indexPairs = matchFeatures(featuresOriginal, featuresDistorted);
 matchedOriginal  = validPtsOriginal(indexPairs(:,1));
 matchedDistorted = validPtsDistorted(indexPairs(:,2));
 
-% 2.2 Robust estimation (MSAC) [2]
+% 3.2 Robust estimation (MSAC) [2]
 
 tform = estimateGeometricTransform2D(...
-    matchedDistorted, matchedOriginal, 'similarity');
+    matchedDistorted, matchedOriginal, 'similarity', 'MaxDistance', 3, 'MaxNumTrials', 10000, 'Confidence', 99.9999);
 
-outputView = imref2d(size(I1_sharp));
-I2_rev  = imwarp(I2_sharp,tform,'OutputView',outputView);
+outputView = imref2d(size(I1));
+I2_rev  = imwarp(I2,tform,'OutputView',outputView);
 
-I1_prepro = I1_sharp;
+I1_prepro = I1;
 I2_prepro = I2_rev;
-
 
 end
 
