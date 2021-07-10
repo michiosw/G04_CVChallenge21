8d61417c7cd91317eef3bd954b8f0f098c5961a0
lalisalala <62295597+lalisalala@users.noreply.github.com>
2021-07-10 21:48:10 +0200

    Update GlacierChangeDetection.m

--- GlacierChangeDetection.m
+++ GlacierChangeDetection.m
@@ -1,7 +1,7 @@
-clear all;
+clear; close all;
 %Specify file path and name
-files = dir('Datasets\ColumbiaGlacier\')
-path = 'Datasets/ColumbiaGlacier/'
+files = dir('Datasets\ColumbiaGlacier\');
+path = 'Datasets/ColumbiaGlacier/';
 %%%Change Detection for Timeline
 for k=4:length(files)
     
@@ -14,12 +14,11 @@ for k=4:length(files)
     %%Show Comparison
     
     %Greyscale
-    ref = rgb2gray(I1_prepro)
-    I2_prepro =  rgb2gray(I2_prepro)
+    ref = rgb2gray(I1_prepro);
+    I2_prepro =  rgb2gray(I2_prepro);
     
     %Binarize Picture
     bwImage=imbinarize(ref,0.7); 
-    figure, imshow(bwImage);
     bwImage2=imbinarize(I2_prepro,0.7); 
 
     
@@ -27,7 +26,7 @@ for k=4:length(files)
     i = imfuse(bwImage,bwImage2,"ColorChannels","red");
     dif = imfuse(I1_prepro,i,'blend','Scaling', 'joint');
     %Saving
-    dif_name = erase(files(k).name, '.jpg')
-    imwrite(dif, [dif_name, '_glacier_dif.jpg'])
+    dif_name = erase(files(k).name, '.jpg');
+    imwrite(dif, [dif_name, '_glacier_dif.jpg']);
     
 end
