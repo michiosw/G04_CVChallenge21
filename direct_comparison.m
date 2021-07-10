89a5c42c1920f90dda02efe0b9f91581dc10e92a
kerstin99forster <72085426+kerstin99forster@users.noreply.github.com>
2021-07-10 12:39:00 +0200

    Upload of change detection functions for first GUI test

--- direct_comparison.m
+++ direct_comparison.m
@@ -7,27 +7,25 @@ I1 = imread(I1_name);
 I2 = imread(I2_name);
 
 switch(environment)
-    case 'Forest'
+    case 'Brazilian Rainforest'
         [I1_prepro, I2_prepro] = preprocessing_forest(I1, I2);
-        change_detection_forest(I1_prepro, I2_prepro);
+        change_detection_forest(1, I1_prepro, I2_prepro, I1_name, I2_name);
     case 'Glacier'
         [I1_prepro, I2_prepro] = preprocessing_glacier(I1, I2);
-        change_detection_glacier(I1_prepro, I2_prepro);
-    case 'City'
-        [I1_prepro, I2_prepro] = preprocessing_city(I1, I2);
-        change_detection_city(I1_prepro, I2_prepro);
-    case 'Sea'
-        [I1_prepro, I2_prepro] = preprocessing_sea(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
-    case 'Waters'
-        [I1_prepro, I2_prepro] = preprocessing_waters(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
+        change_detection_glacier(1, I1_prepro, I2_prepro, I1_name, I2_name);
     case 'Tire Graveyard'
-        [I1_prepro, I2_prepro] = preprocessing_tiregraveyard(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
-    case 'Solar Plant'
-        [I1_prepro, I2_prepro] = preprocessing_solarplant(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
+        [I1_prepro, I2_prepro] = preprocessing_tire(I1, I2);
+        change_detection_tire(1, I1_prepro, I2_prepro, I1_name, I2_name);
+    case 'Dubai'
+        change_detection_dubai(path);
+    case 'Kuwait'
+        change_detection_kuwait(path);
+    case 'Frauenkirche'
+        [I1_prepro, I2_prepro] = preprocessing_frauenkirche(I1, I2);
+        change_detection_frauenkirche(I1_prepro, I2_prepro, I1_name, I2_name);
+    case 'Wiesn'
+        [I1_prepro, I2_prepro] = preprocessing_wiesn(I1, I2);
+        change_detection_wiesn(I1_prepro, I2_prepro, I1_name, I2_name);
 end
 
 end
--- sequential_comparison.m
+++ sequential_comparison.m
@@ -5,32 +5,62 @@ function sequential_comparison(path, environment)
 addpath(path);
 images = dir([path '/*.jpg']);
 num_images = size(images, 1);
-I1 = imread(images(1).name);
+I1_name = images(1).name;
+I1 = imread(I1_name);
 
 switch(environment)
+    case 'Brazilian Rainforest'
+        for i = 1:(num_images-1)
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_rainforest(i, I1, I2);
+            % only for development purposes
+            %figure, imshowpair(I1_prepro, I2_prepro,'montage')
+            change_detection_rainforest(i, I1_prepro, I2_prepro, I1_name, I2_name);
+        end    
     case 'Forest'
         for i = 1:(num_images-1)
-            I2 = imread(images(i+1).name);
-            [I1_prepro, I2_prepro] = preprocessing_forest(I1, I2);
-            %change_detection_forest(I1_prepro, I2_prepro);
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_frauenkirche(i, I1, I2);
+            % only for development purposes
+            %figure, imshowpair(I1_prepro, I2_prepro,'montage')
+            change_detection_forest(i, I1_prepro, I2_prepro, I1_name, I2_name);
         end
     case 'Glacier'
-        [I1_prepro, I2_prepro] = preprocessing_glacier(I1, I2);
-        change_detection_glacier(I1_prepro, I2_prepro);
-    case 'City'
-        [I1_prepro, I2_prepro] = preprocessing_city(I1, I2);
-        change_detection_city(I1_prepro, I2_prepro);
-    case 'Sea'
-        [I1_prepro, I2_prepro] = preprocessing_sea(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
-    case 'Waters'
-        [I1_prepro, I2_prepro] = preprocessing_waters(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
+        for i = 1:(num_images-1)
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_glacier(I1, I2);
+            change_detection_glacier(i, I1_prepro, I2_prepro, I1_name, I2_name);
+        end
     case 'Tire Graveyard'
-        [I1_prepro, I2_prepro] = preprocessing_waters(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro);
-    case 'Solar Plant'
-        [I1_prepro, I2_prepro] = preprocessing_waters(I1, I2);
-        change_detection_sea(I1_prepro, I2_prepro); 
+        for i = 1:(num_images-1)
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_tire(I1, I2);
+            if i == 1
+                ref = change_detection_tire_ref(I1_prepro, I1_name);
+            end
+            change_detection_tire(I1_prepro, I2_prepro, I2_name, ref);
+        end
+    case 'Dubai'
+        change_detection_dubai(path);
+    case 'Kuwait'
+        change_detection_kuwait(path);
+    case 'Frauenkirche'
+        for i = 1:(num_images-1)
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_frauenkirche(I1, I2);
+            change_detection_frauenkirche(i, I1_prepro, I2_prepro, I1_name, I2_name);
+        end
+    case 'Wiesn'
+        for i = 1:(num_images-1)
+            I2_name = images(i+1).name;
+            I2 = imread(I2_name);
+            [I1_prepro, I2_prepro] = preprocessing_wiesn(I1, I2);
+            change_detection_wiesn(i, I1_prepro, I2_prepro, I1_name, I2_name);
+        end
 end
 
